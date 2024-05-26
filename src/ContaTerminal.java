ac8f03f8b87ccc13b6d3961efc1e262832b1edc3
marcospsantos226 <marcospsantos226@gmail.com>
2024-05-26 17:20:29 -0300

    Meu banco no terminal completo

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    1021
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo = 0.0;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta:");
+        numero = entrada.nextInt();
+        entrada.nextLine(); // Consumir a quebra de linha após a entrada do número
+
+        System.out.println("Por favor, digite o número da Agência!");
+        agencia = entrada.nextLine(); // Lê a linha inteira
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        nomeCliente = entrada.nextLine(); // Lê a linha inteira
+
+        System.out.println("Por favor, digite o saldo do cliente:");
+        saldo = entrada.nextDouble(); // Lê o valor float
+
+        
+
+        // Formatar a saída para melhor leitura
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
